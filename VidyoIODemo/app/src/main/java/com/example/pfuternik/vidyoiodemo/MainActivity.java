f03487975d1af01a60878af1755656c1bb024cfb
Andrii.Salii <tehnicyss@gmail.com>
2021-02-16 15:26:39 +0200

    Add request for camera permission

--- VidyoIODemo/app/src/main/java/com/example/pfuternik/vidyoiodemo/MainActivity.java
+++ VidyoIODemo/app/src/main/java/com/example/pfuternik/vidyoiodemo/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.pfuternik.vidyoiodemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
-import com.vidyo.VidyoClient.Connector.ConnectorPkg;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.vidyo.VidyoClient.Connector.Connector;
+import com.vidyo.VidyoClient.Connector.ConnectorPkg;
 
 public class MainActivity extends AppCompatActivity implements Connector.IConnect {
 
     private Connector vc;
     private FrameLayout videoFrame;
+    private static final int CAMERA_PERMISSION_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,42 @@ public class MainActivity extends AppCompatActivity implements Connector.IConnec
         videoFrame = (FrameLayout)findViewById(R.id.videoFrame);
     }
 
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return;
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    MainActivity.CAMERA_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(MainActivity.this,
+                        R.string.permissions,
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
     public void Start(View v) {
+        checkPermission();
         vc = new Connector(videoFrame, Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default, 15, "warning info@VidyoClient info@VidyoConnector", "", 0);
         vc.showViewAt(videoFrame, 0, 0, videoFrame.getWidth(), videoFrame.getHeight());
     }
